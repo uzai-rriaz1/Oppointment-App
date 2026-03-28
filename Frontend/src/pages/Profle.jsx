e4b0ced3ac70f14d97ca6d35aa4a5bb233f4cd74
uzai_rriaz1 <uzairriaz1296@gmail.com>
2026-03-29 00:19:26 +0500

    Updated Profile Page

--- Frontend/src/pages/Profle.jsx
+++ Frontend/src/pages/Profle.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { CircleUserRound } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 const Profle = () => {
+  const navigate = useNavigate();
   return (
     <table className="flex flex-col  items-center justify-center min-h-screen ">
       <tr className="flex flex-col gap-10 items-center justify-center bg-blue-500 w-md h-96 rounded-xl">
@@ -11,7 +13,10 @@ const Profle = () => {
         <td className="text-white text-xl font-bold ">
           uzairriaz@123gmail.com
         </td>
-        <button className="bg-white text-blue-500 text-xl rounded-2xl p-4 hover:scale-105 transition-transform">
+        <button
+          onClick={() => navigate("/reset")}
+          className="bg-white text-blue-500 text-xl rounded-2xl p-4 hover:scale-105 transition-transform"
+        >
           Reset Password
         </button>
       </tr>
