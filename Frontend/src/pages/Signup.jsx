636b432a3c21da5236c8eab661d29ca864c9a3cb
uzai-rriaz1 <uzairriaz1296@gmail.com>
2026-04-27 19:10:27 +0500

    signup page changes and better error handling

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -8,28 +8,48 @@ const Signup = () => {
   const navigate = useNavigate();
   const [showpass, setShowpass] = useState(false);
   const [eyeopen, setEyeopen] = useState(false);
+  const [role, setRole] = useState("patient");
 
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors },
   } = useForm();
 
   const onSubmit = async (data) => {
     try {
-      const response = await routeApi.post("/users/createuser", data);
+      const payload = {
+        name: data.username,
+        email: data.email,
+        password: data.password,
+        role: role,
+        ...(role === "doctor" && {
+          specialization: data.specialization.trim(),
+        }),
+      };
+
+      const response = await routeApi.post("/users/createuser", payload, {
+        withCredentials: true,
+      });
+
       if (response) {
-        console.log("Data sended succsfully ", response);
+        console.log("Data sent successfully", response);
+        navigate("/signin");
       }
     } catch (error) {
-      throw new Error("Data didnt sended", error);
+      console.log("FULL ERROR:", error.response);
+      setError("root", {
+        type: "server",
+        message: error.response?.data?.message || "Something went wrong",
+      });
     }
   };
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="min-h-screen flex items-center justify-center bg-gray-50 px-4"
+      className="min-h-screen flex items-center justify-center bg-gray-50 px-4 my-15"
     >
       <div className="w-full max-w-md bg-white rounded-2xl shadow-lg p-6 sm:p-8 space-y-6">
         <div className="text-center">
@@ -38,10 +58,24 @@ const Signup = () => {
             Create your account to get started
           </p>
         </div>
+        {errors.root && (
+          <p className="text-red-500 text-sm text-center mt-1">
+            {errors.root.message}
+          </p>
+        )}
+        <div>
+          <select
+            className="w-full p-3 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-teal-500"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+          >
+            <option value="patient">Patient</option>
+            <option value="doctor">Doctor</option>
+          </select>
+        </div>
 
         <div>
           <input
-            id="username"
             type="text"
             placeholder="Enter your username"
             className="w-full p-3 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-teal-500"
@@ -58,7 +92,6 @@ const Signup = () => {
 
         <div>
           <input
-            id="email"
             type="email"
             placeholder="Enter your email"
             className="w-full p-3 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-teal-500"
@@ -75,9 +108,26 @@ const Signup = () => {
           )}
         </div>
 
+        {role === "doctor" && (
+          <div>
+            <input
+              type="text"
+              placeholder="Enter your specialization"
+              className="w-full p-3 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-teal-500"
+              {...register("specialization", {
+                required: "Enter specialization",
+              })}
+            />
+            {errors.specialization && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.specialization.message}
+              </p>
+            )}
+          </div>
+        )}
+
         <div className="relative">
           <input
-            id="password"
             type={showpass ? "text" : "password"}
             placeholder="Enter your password"
             className="w-full p-3 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-teal-500"
@@ -86,7 +136,7 @@ const Signup = () => {
               pattern: {
                 value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
                 message:
-                  "Enter at least 1 letter, 1 number and min 8 characters",
+                  "Enter at least 1 letter, 1 number, No special Characters and min 8 characters",
               },
             })}
           />
