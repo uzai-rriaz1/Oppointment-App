da2f02281767f970af0fb84b5dee7d4c3964e771
uzai-rriaz1 <uzairriaz1296@gmail.com>
2026-04-30 22:34:22 +0500

    added doctor availability controller for setting availability slots

--- Backend/src/controllers/doctor.controller.js
+++ Backend/src/controllers/doctor.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import { apiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const doctorsList = asyncHandler(async (req, res, next) => {
@@ -11,3 +12,31 @@ const doctorsList = asyncHandler(async (req, res, next) => {
 });
 
 export { doctorsList };
+
+const doctorAvailibility = asyncHandler(async (req, res, next) => {
+  const { id, day, slots } = req.body;
+  if (!day || !slots || !id)
+    throw new apiError(401, "Please Enter Slots And Days");
+
+  const user = await User.findById(id);
+
+  if (!user) throw new apiError(402, "User Id isnt available");
+
+  if (user.role !== "doctor")
+    throw new apiError(403, "Only Doctor Can Set Slots");
+  const existingDay = user.availability.find((d) => d.day === day);
+  if (existingDay) {
+    existingDay.slots = slots;
+  } else {
+    await user.availability.push({ days, slots });
+  }
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    Availability: "Availability Updated Suuccessfully",
+    availability: user.availability,
+  });
+});
+
+export { doctorAvailibility };
