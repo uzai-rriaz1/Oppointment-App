30f314aebd329d8bb963be092b571584dc739fed
Uzair Riaz <uzairriaz1296@gmail.com>
2026-05-12 18:51:59 +0500

    Update api.js

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const routeApi = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "http://sunny-playfulness-production-4174.up.railway.app/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
